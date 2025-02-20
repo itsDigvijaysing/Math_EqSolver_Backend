377dc70640657688652923638af7c4d18acca85b
itsdigvijaysing <itsdigvijaysing@gmail.com>
2025-02-20 21:58:01 +0530

    some changes

--- backend/settings.py
+++ backend/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'corsheaders',
 ]
 
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
